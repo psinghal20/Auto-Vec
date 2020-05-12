580a6b026e7f83e5dc3ccf085a41267ff6c227b4
psinghal20 <psinghal20@gmail.com>
2020-05-12 23:14:03 +0530

    feat: allow methods to be vectorized using auto vec
    Allow vectorization of methods(functions receving self as argument) accepting vectors for all arguments except self.
    
    Signed-off-by: psinghal20 <psinghal20@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -27,25 +27,31 @@ pub fn auto_vec(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     // Extract inputs from function signature
     let inputs = scalar.sig.inputs.iter().map(|f| {
-        if let syn::FnArg::Typed(arg) = f {
-            let arg_ident = arg.pat.clone();
-            let ty  = &arg.ty;
-            return quote!{ mut #arg_ident: Vec<#ty> };
-        } else {
-            syn::Error::new_spanned(f, "Expected typed arguments, found untyped argument").to_compile_error()
+        match f {
+            syn::FnArg::Typed(arg) => {
+                let arg_ident = arg.pat.clone();
+                let ty  = &arg.ty;
+                quote!{ mut #arg_ident: Vec<#ty> }
+            }
+            syn::FnArg::Receiver(recv) =>  quote! { #recv }
         }
     });
 
     // Extract input argument idents from the function signature 
-    let input_idents = scalar.sig.inputs.iter().map(|input| {
+    let input_idents: Vec<_> = scalar.sig.inputs.iter().filter_map(|input| {
         if let syn::FnArg::Typed(arg) = input {
             let pattern = arg.pat.clone();
             let expr = quote! { #pattern };
-            expr
+            Some(expr)
         } else {
-            syn::Error::new_spanned(input, "Expected typed arguments, found untyped self argument").to_compile_error()
+            None
+            // syn::Error::new_spanned(input, "Expected typed arguments, found untyped self argument").to_compile_error()
         }
-    });
+    }).collect();
+
+    if input_idents.len() == 0 {
+        return syn::Error::new_spanned(sig_clone_for_error, "Auto_vec: Expected one or more Typed arguments, Found None").to_compile_error().into();
+    }
 
     // Copy of idents used for function call
     let input_idents_for_function_call = input_idents.clone();
@@ -53,8 +59,8 @@ pub fn auto_vec(_args: TokenStream, input: TokenStream) -> TokenStream {
     let input_idents_for_len_assertion = input_idents.clone();
     let mut input_idents_for_len_assestion_next = input_idents.clone();
 
-    // Move the copy one element forward for assert_eq
-    input_idents_for_len_assestion_next.next();
+    // Remove the first element from typed inputs for assert_eq
+    input_idents_for_len_assestion_next.remove(0);
 
     // Extract first input ident to be used for for loop
     let first_input_ident = input_idents.clone().into_iter().next();
@@ -69,13 +75,19 @@ pub fn auto_vec(_args: TokenStream, input: TokenStream) -> TokenStream {
         }
     };
 
+    let function_caller = if let syn::FnArg::Receiver(_) = scalar.sig.inputs.first().unwrap() {
+        quote! { self.#name }
+    } else {
+        quote! { #name }
+    };
+
     // Generated extended method to take vectorized inputs
     let extended_method = quote! {
         #visibility fn #vec_ident#generics(#(#inputs,)*) -> #outputs {
-            #(assert_eq!(#input_idents_for_len_assertion.len(), #input_idents_for_len_assestion_next.len(), "Input vectors of not same length");)*
+            #(assert_eq!(#input_idents_for_len_assertion.len(), #input_idents_for_len_assestion_next.len(), "Input vectors of not the same length to vectorized function {}", #vec_name);)*
             let mut result = vec![];
             for i in 0..#first_input_ident.len() {
-                result.push(#name(#(#input_idents_for_function_call.remove(0),)*));
+                result.push(#function_caller(#(#input_idents_for_function_call.remove(0),)*));
             }
             return result;
         }
